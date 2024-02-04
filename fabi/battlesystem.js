b962fbae0011e23e038fe6cd8e6651ade4726a0d
Freeman010 <fabi.sasse@web.de>
2024-02-04 19:44:36 +0100

    kampfsystem kann jetzt eine flotte eine andere angreifen lassen , abwechselnder kampf fehlt noch

--- fabi/battlesystem.js
+++ fabi/battlesystem.js
@@ -1,23 +1,206 @@
 import Unit from './unit.js';
-
-export default class Battlesystem
-{
-    constructor()
-    {
+import unitdata from './unitdata.js';
+export default class Battlesystem {
+    constructor() {
 
     }
-    attackunit(myunit, attackedunit)
-    {
-        if(myunit instanceof Unit && attackedunit instanceof Unit)
-        {
-            console.log("Hülle vorher: " + attackedunit.hull);
-            attackedunit.sethull(attackedunit.hull - myunit.firepower);
-            
-            console.log("Hülle nacher: " + attackedunit.hull);
+    searchtarget(defenderlist) {
+        if (Array.isArray(defenderlist)) {
+            if (defenderlist.length > 0) {
+                let targetnumber = Math.floor(Math.random() * defenderlist.length);
+                return defenderlist[targetnumber];
+            }
 
         }
     }
+    handlefight(myunit, defenderlist, target, dmgversus, rapidfirevs) {
+        
+        console.log(myunit.name + " greift " + target.name + " an");
+        if (myunit instanceof Unit && target instanceof Unit) {
+            if (rapidfirevs != 1) {
+                while (myunit.attackenergy >= 0) {
+                    console.log(myunit.name + " hat rapidfire von " + rapidfirevs + " gegen " + target.name);
+                    console.log("der " + target.name + " hat aktuell eine hülle von " + target.hull);
+                    console.log(myunit.name + " feuert mit einer feuerkraft von ", myunit.firepower + dmgversus, "auf den ", target.name);
+                    target.shield = target.shield - myunit.firepower + dmgversus;
+                    
+                    if (target.shield <= 0) {
+                        target.hull += target.shield;
+                        target.shield = 0;
+                        console.log("angreifer " + myunit.name, "hat den schild durchbrochen");
+
+                        if (target.hull <= 0) {
+                            
+                            defenderlist.splice(defenderlist.indexOf(target), 1);
+                            console.log(target.name + " wurde zerstört");
+                            break;
+                        }
+                        console.log(target.name + " hat noch " + target.hull + " hüllenpunkte bevor er zerstört wird");
+                    }
+                    else {
+                        console.log(target.name + " hat noch " + target.shield + " schildpunkte bevor der schild bricht");
+                    }
+                    myunit.attackenergy = myunit.attackenergy - 100 / rapidfirevs;
+                    if(myunit.attackenergy <= 0){break;}
+                }
+            }
+            else {
+                target.shield = target.shield - myunit.firepower + dmgversus;
+                console.log(myunit.name + " feuert mit einer feuerkraft von ", myunit.firepower + dmgversus, " auf den ", target.name);
+                if (target.shield <= 0) {
+                    target.hull = target.hull + target.shield;
+                    target.shield = 0;
+                    console.log(target.shield+"schild");
+                    console.log(target.hull+" hülle")
+                    if (target.hull <= 0) {
+                        console.log(target.name + " wurde zerstört")
+                        defenderlist.splice(target, 1);
+                        
+                    }
+                    else{
+                        console.log(target.name + "hat noch " + target.hull + " hüllenpunkte bevor er zerstört wird");
+                    }
+                }
+                else {
+                    console.log(target.name + "hat noch " + target.shield + " schildpunkte bevor der schild bricht");
+                }
+                
+                
+                myunit.attackenergy = myunit.attackenergy - 100 / rapidfirevs;
+            }
+        }
+    }
+    attackunit(myunit, defenderlist) {
+
+        if (myunit instanceof Unit && Array.isArray(defenderlist)) {
+            while (myunit.attackenergy >= 0) {
+                if (defenderlist.length <= 0) {
+                    break;
+                }
+                if(myunit.attackenergy <= 0)
+                {
+                    break;
+                }
+                let target = this.searchtarget(defenderlist)
+                switch (target.unittype) {
+
+                    case Unit.unittype.leichterjaeger: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusleichterjaeger, myunit.rapidfirevsleichterjaeger);
+                        break;
+                    }
+                    case Unit.unittype.schwererjaeger: {
+
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusschwererjaeger, myunit.rapidfirevsschwererjaeger);
+                        break;
+                    }
+                    case Unit.unittype.bomber: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusbomber, myunit.rapidfirevsbomber);
+                        break;
+                    }
+                    case Unit.unittype.fregatte: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusfregatte, myunit.rapidfirevsfregatte);
+                        break;
+                    }
+                    case Unit.unittype.kleinertransporter: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusartillerie,dmgversuskleinertransporter, myunit.rapidfirevskleinertransporter);
+                        break;
+                    }
+                    case Unit.unittype.miningdrohne: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusminingdrohne, myunit.rapidfirevsminingdrohne);
+                        break;
+                    }
+                    case Unit.unittype.grossertransporter: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusgrossertransporter, myunit.rapidfirevsschwererjaeger);
+                        break;
+                    }
+                    case Unit.unittype.zerstoerer: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversuszerstörer, myunit.rapidfirevszerstörer);
+                        break;
+                    }
+                    case Unit.unittype.kreuzer: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversuskreuzer, myunit.rapidfirevskreuzer);
+                        break;
+                    }
+                    case Unit.unittype.flugdeckkreuzer: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusflugdeckkreuzer, myunit.rapidfirevsflugdeckkreuzer);
+                        break;
+                    }
+                    case Unit.unittype.kolonieschiff: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversuskolonieschiff, myunit.rapidfirevskolonieschiff);
+                        break;
+                    }
+                    case Unit.unittype.bergbauschiff: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusbergbauschiff, myunit.rapidfirevsbergbauschiff);
+                        break;
+                    }
+                    case Unit.unittype.schlachtschiff: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusschlachtschiff, myunit.rapidfirevsschlachtschiff);
+                        break;
+                    }
+                    case Unit.unittype.schlachtkreuzer: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusschlachtkreuzer, myunit.rapidfirevsschlachtkreuzer);
+                        break;
+                    }
+                    case Unit.unittype.traegerschiff: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusträgerschiff, myunit.rapidfirevsträgerschiff);
+                        break;
+                    }
+                    case Unit.unittype.flakgeschuetz: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusflakgeschütz, myunit.rapidfirevsflakgeschütz);
+                        break;
+                    }
+                    case Unit.unittype.artillerie: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusartillerie, myunit.rapidfirevsartillerie);
+                        break;
+                    }
+                    case Unit.unittype.ionenkanone: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusionenkanone, myunit.rapidfirevsionenkanone);
+                        break;
+                    }
+                    case Unit.unittype.lasergeschuetz: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversuslasergeschütz, myunit.rapidfirevslasergeschütz);
+                        break;
+                    }
+                    case Unit.unittype.railgun: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusrailgun, myunit.rapidfirevsrailgun);
+                        break;
+                    }
+                    case Unit.unittype.partikelkanone: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversuspartikelkanone, myunit.rapidfirevspartikelkanone);
+                        break;
+                    }
+                    case Unit.unittype.planetarerschildgenerator: {
+                        this.handlefight(myunit, defenderlist, target, myunit.dmgversusplanetarerschildgenerator, myunit.rapidfirevsplanetarerschildgenerator);
+                        break;
+                    }
+                    default: {
+                        // Code für den Fall, das nichts zutrifft
+                    }
+                }
+            }
+        }
+    }
+    startwar(attacker, defender) {
+        if (Array.isArray(attacker) && Array.isArray(defender)) {
+            let attackingships = [...attacker];
+            let shipfinishedattack = [];
+
+            for (let i = 0; i < attacker.length; i++) {
+                let attackershipnumber = Math.floor(Math.random() * attackingships.length);
+
+                if (defender.length <= 0) { break; }
+                this.attackunit(attackingships[attackershipnumber], defender);
+                shipfinishedattack.push(attackingships[attackershipnumber]);
+                
+                console.log(defender.length);
+                attackingships.splice(attackershipnumber, 1);
+
+            }
+        }
+    }
+
 }
+// node unitdata.js
 /*export default function attackunit(myunit, attackedunit)
 {
     if(myunit instanceof Unit&& attackedunit instanceof Unit)
--- fabi/unit.js
+++ fabi/unit.js
@@ -1,123 +1,178 @@
 export default class Unit {
+  static attackenergy;
+  static name;
+  static steelcosts;
+  static mikroshipkosten;
+  static chemicalcost;
+  static energycosts;
+  static firepower;
+  static hull;
+  static shield;
+  static speed;
+  static fuelconsume;
+  static ammoconsume;
+  static hangaring // if negative the unit use hangarslots, if positive unit have hangarslots
+  static cargo;
+  static unittype;
+
+  
+
+  static unittype = {
+    leichterjaeger: 1,
+    schwererjaeger: 2,
+    bomber: 3,
+    fregatte: 4,
+    kleinertransporter: 5,
+    miningdrohne: 6,
+
+    grossertransporter: 7,
+    zerstörer: 8,
+    kreuzer: 9,
+    flugdeckkreuzer: 10,
+    kolonieschiff: 11,
+    bergbauschiff: 12,
+
+    schlachtschiff: 13,
+    schlachtkreuzer: 14,
+    trägerschiff: 15,
+
+    flakgeschuetz: 16,
+    artillerie: 17,
+    ionenkanone: 18,
+    lasergeschuetz: 19,
+    railgun: 20,
+    partikelkanone: 21,
+    planetarerschildgenerator: 22
+  }
+
+  dmgversusleichterjaeger = 0;
+  dmgversusschwererjaeger = 0;
+  dmgversusbomber = 0;
+  dmgversusfregatte = 0;
+  dmgversuskleinertransporter = 0;
+  dmgversusminingdrohne = 0;
+
+  dmgversusgrossertransporter = 0;
+  dmgversuszerstörer = 0;
+  dmgversuskreuzer = 0;
+  dmgversusflugdeckkreuzer = 0;
+  dmgversuskolonieschiff = 0;
+  dmgversusbergbauschiff = 0;
+
+  dmgversusschlachtschiff = 0;
+  dmgversusschlachtkreuzer = 0;
+  dmgversusträgerschiff = 0;
+
+  dmgversusflakgeschütz = 0;
+  dmgversusartillerie = 0;
+  dmgversusionenkanone = 0;
+  dmgversuslasergeschütz = 0;
+  dmgversusrailgun = 0;
+  dmgversuspartikelkanone = 0;
+  dmgversusplanetarerschildgenerator = 0;
+
+  ///Rapidfire
+  rapidfirevsleichterjaeger = 1;
+  rapidfirevsschwererjaeger = 1;
+  rapidfirevsbomber = 1;
+  rapidfirevsfregatte = 1;
+  rapidfirevskleinertransporter = 1;
+  rapidfirevsminingdrohne = 1;
+
+  rapidfirevsgroßertransporter = 1;
+  rapidfirevszerstörer = 1;
+  rapidfirevskreuzer = 1;
+  rapidfirevsflugdeckkreuzer = 1;
+  rapidfirevskolonieschiff = 1;
+  rapidfirevsbergbauschiff = 1;
+
+  rapidfirevsschlachtschiff = 1;
+  rapidfirevsschlachtkreuzer = 1;
+  rapidfirevsträgerschiff = 1;
+
+  rapidfirevsflakgeschütz = 1;
+  rapidfirevsartillerie = 1;
+  rapidfirevsionenkanone = 1;
+  rapidfirevslasergeschütz = 1;
+  rapidfirevsrailgun = 1;
+  rapidfirevspartikelkanone = 1;
+  rapidfirevsplanetarerschildgenerator = 1;
 
 
-    static steelcosts;
-    static chemicalcost;
-    static energycosts;
-    static firepower;
-    static hull;
-    static shield;
-    static speed;
-    static fuelconsume;
-    static ammoconsume;
-    static hangaring // if negative the unit use hangarslots, if positive unit have hangarslots
-    static cargo;
-    static unittype;
-
-    static unittype = {
-        leichterjäger: 1,
-        schwererjäger: 2,
-        bomber: 3,
-        fregatte: 4,
-        kleinertransporter: 5,
-        miningdrohne: 6,
-
-        großertransporter: 7,
-        zerstörer: 8,
-        kreuzer: 9,
-        flugdeckkreuzer: 0,
-        kolonieschiff: 11,
-        bergbauschiff: 12,
-
-        schlachtschiff: 13,
-        schlachtkreuzer: 14,
-        trägerschiff: 15,
-
-        flakgeschütz: 16,
-        artillerie: 17,
-        ionenkanone: 18,
-        lasergeschütz: 19,
-        railgun: 20,
-        partikelkanone: 21,
-        planetarerschildgenerator: 22
-    }
-
-
-get(propertyName) {
+  get(propertyName) {
     return this[propertyName];
-}
+  }
 
-set(propertyName, value) {
+  set(propertyName, value) {
     this[propertyName] = value;
-}
+  }
 
-getsteelcost(){
+  getsteelcost() {
     return this.steelcosts;
-}
-setsteelcost(value)
-{
+  }
+  setsteelcost(value) {
     this.steelcosts = value;
-}
-getchemicalcosts(){
+  }
+  getchemicalcosts() {
     return this.chemicalcost;
-}
-setchemicalcosts(value){
+  }
+  setchemicalcosts(value) {
     this.chemicalcost = value;
-}
-getenergycosts(){
+  }
+  getenergycosts() {
     return this.energycosts;
-}
-setenergycosts(value){
+  }
+  setenergycosts(value) {
     this.energycosts = value;
-}
-getfirepower(){
+  }
+  getfirepower() {
     return this.firepower;
-}
-setfirepower(value){
+  }
+  setfirepower(value) {
     this.firepower = value;
-}
-gethull(){
+  }
+  gethull() {
     return this.hull;
-}
-sethull(value){
+  }
+  sethull(value) {
     this.hull = value;
-}
-getcshield(){
+  }
+  getcshield() {
     return this.shield;
-}
-setshield(value){
+  }
+  setshield(value) {
     this.shield = value;
-}
-getfuelconsume(){
+  }
+  getfuelconsume() {
     return this.fuelconsume;
-}
-setfuelconsume(value){
+  }
+  setfuelconsume(value) {
     this.fuelconsume = value;
-}
-getammoconsume(){
+  }
+  getammoconsume() {
     return this.ammoconsume;
-}
-setamoconsume(value){
+  }
+  setamoconsume(value) {
     this.ammoconsume = value;
-}
-gethangar(){
+  }
+  gethangar() {
     return this.hangaring;
-}
-sethangar(value){
+  }
+  sethangar(value) {
     this.hangaring = value;
-}
-getcargo(){
+  }
+  getcargo() {
     return this.cargo;
-}
-setcargo(value){
+  }
+  setcargo(value) {
     this.cargo = value;
-}
-getunittype(){
+  }
+  getunittype() {
     return this.unittype;
-}
-setcunittype(value){
+  }
+  setcunittype(value) {
     this.unittype = value;
-}
+  }
 }
 /*
   static chemicalcost;
